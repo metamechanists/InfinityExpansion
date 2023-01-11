5f91a9be473f0f5a09b6c925e8a0b467ac187eb9
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2023-01-11 10:34:25 -0600

    Update MobDataInfuser.java

--- src/main/java/io/github/mooy1/infinityexpansion/items/mobdata/MobDataInfuser.java
+++ src/main/java/io/github/mooy1/infinityexpansion/items/mobdata/MobDataInfuser.java
@@ -1,13 +1,5 @@
 package io.github.mooy1.infinityexpansion.items.mobdata;
 
-import javax.annotation.Nonnull;
-import javax.annotation.ParametersAreNonnullByDefault;
-
-import org.bukkit.ChatColor;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-
 import io.github.mooy1.infinitylib.machines.CraftingBlock;
 import io.github.mooy1.infinitylib.machines.MachineRecipeType;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
@@ -16,11 +8,18 @@ import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.attributes.EnergyNetComponent;
 import io.github.thebusybiscuit.slimefun4.core.networks.energy.EnergyNetComponentType;
 import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
+import org.bukkit.ChatColor;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
 public final class MobDataInfuser extends CraftingBlock implements EnergyNetComponent {
 
-    static final MachineRecipeType TYPE = new MachineRecipeType("mob_data_infuser", MobData.INFUSER);
+    public static final MachineRecipeType TYPE = new MachineRecipeType("mob_data_infuser", MobData.INFUSER);
 
     private final int energy;
 
