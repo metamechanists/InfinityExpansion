08b3b78cdfc4aab68b4a6c74e2bc1d2bcc58d2a4
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2024-03-21 12:19:44 -0500

    add geo resource display

--- src/main/java/io/github/mooy1/infinityexpansion/items/machines/GeoQuarry.java
+++ src/main/java/io/github/mooy1/infinityexpansion/items/machines/GeoQuarry.java
@@ -11,7 +11,9 @@ import io.github.thebusybiscuit.slimefun4.core.attributes.RecipeDisplayItem;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.collections.Pair;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.collections.RandomizedSet;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.common.ChatColors;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
+import io.github.thebusybiscuit.slimefun4.utils.ChatUtils;
 import lombok.Setter;
 import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
 import me.mrCookieSlime.Slimefun.api.inventory.BlockMenuPreset;
@@ -23,6 +25,7 @@ import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -33,6 +36,13 @@ public final class GeoQuarry extends AbstractMachineBlock implements RecipeDispl
 
     private static final int STATUS = 4;
     private static final int[] OUTPUT_SLOTS = { 29, 30, 31, 32, 33, 38, 39, 40, 41, 42 };
+    private static final Map<World.Environment, String> ENVIRONMENT_NAMES = Map.of(
+            World.Environment.NORMAL, "&a   The Overworld",
+            World.Environment.NETHER, "&c   The Nether",
+            World.Environment.THE_END, "&d   The End",
+            World.Environment.CUSTOM, "&b   Custom"
+    );
+    private static final Map<GEOResource, List<String>> GEO_RESOURCE_AVAILABILITY = new HashMap<>();
 
     private final Map<Pair<Biome, World.Environment>, RandomizedSet<ItemStack>> recipes = new HashMap<>();
     @Setter
@@ -120,11 +130,67 @@ public final class GeoQuarry extends AbstractMachineBlock implements RecipeDispl
 
         for (GEOResource resource : Slimefun.getRegistry().getGEOResources().values()) {
             if (resource.isObtainableFromGEOMiner()) {
-                displayRecipes.add(resource.getItem());
+                if (!GEO_RESOURCE_AVAILABILITY.containsKey(resource)) {
+                    cacheResourceAvailability(resource);
+                }
+
+                displayRecipes.add(new CustomItemStack(resource.getItem(), meta -> {
+                    final List<String> lore = meta.hasLore() ? meta.getLore() : new ArrayList<>();
+                    lore.add(" ");
+                    lore.addAll(GEO_RESOURCE_AVAILABILITY.get(resource));
+                    meta.setLore(lore);
+                }));
             }
         }
 
         return displayRecipes;
+    }
+
+    private void cacheResourceAvailability(GEOResource resource) {
+        final List<String> availability = new ArrayList<>();
+        for (World.Environment environment : World.Environment.values()) {
+            boolean all = true;
+            boolean added = false;
+            final StringBuilder biomes = new StringBuilder();
+            final List<String> toAdd = new ArrayList<>();
+            for (Biome biome : Biome.values()) {
+                if (resource.getDefaultSupply(environment, biome) > 0) {
+                    if (!added) {
+                        availability.add(ChatColors.color(ENVIRONMENT_NAMES.get(environment)));
+                    }
+
+                    if (biomes.length() >= 30) {
+                        toAdd.add(ChatColors.color(biomes.toString()));
+                        biomes.setLength(0);
+                    }
+
+                    if (biomes.isEmpty()) {
+                        biomes.append("&f     ");
+                    } else {
+                        biomes.append(", ");
+                    }
+
+                    biomes.append(ChatUtils.humanize(biome.toString()));
+                    added = true;
+                } else {
+                    all = false;
+                }
+            }
+
+            if (!biomes.isEmpty()) {
+                toAdd.add(ChatColors.color(biomes.toString()));
+            }
+
+            if (all) {
+                availability.add(ChatColors.color("&f     Any Biome"));
+            } else {
+                availability.addAll(toAdd);
+            }
+        }
+
+        availability.add(0, ChatColors.color(availability.isEmpty() ? "&cNot Harvestable" : "&eHarvestable In:"));
+
+        GEO_RESOURCE_AVAILABILITY.put(resource, availability);
     }
 
 }
